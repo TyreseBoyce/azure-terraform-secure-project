5bff11c2348ba86d953ccbc86487c08cff1c5ead
TyreseBoyce <tyrese13boyce@gmail.com>
2025-07-24 12:21:07 -0400

    Fix Bug

--- variables.tf
+++ variables.tf
@@ -41,6 +41,7 @@ variable "users" {
     user_principal_name = string
     display_name        = string
     password            = string
+    sensitive = true
   }))
   description = "Map of users to be created with their principal name, display name, and password"
 }
